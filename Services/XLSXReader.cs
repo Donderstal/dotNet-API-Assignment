72ef5b93646436eb07815d0154014f09922a0d98
Donderstal <daanonderstal@hotmail.com>
2021-05-25 10:21:27 +0200

    looping through the excel file's contents

--- Services/XLSXReader.cs
+++ Services/XLSXReader.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Collections.Generic;
+using OfficeOpenXml;
 
 namespace XLSXReaderAPI.Services {
     public class XLSXReader {
@@ -27,7 +28,36 @@ namespace XLSXReaderAPI.Services {
             unsetFileFromReader( );
             setFileToReader( filePath );
 
+            return mapExcelSheets( ref mappedExcelFile );
+        }
+        public Dictionary<string, Dictionary<string, string>> mapExcelSheets( ref Dictionary<string, Dictionary<string, string>> mappedExcelFile ) 
+        {
+            using ( var package = new ExcelPackage( new FileInfo( FilePath )) ) {
+                foreach ( var worksheet in package.Workbook.Worksheets )
+                {
+                    string worksheetKey = worksheet.Name;
+                    mappedExcelFile.Add( worksheetKey, mapExcelTable( worksheet ) );
+                }
+            }
             return mappedExcelFile;
+        }
+
+        public Dictionary<string, string> mapExcelTable( ExcelWorksheet worksheet )
+        {
+            List<string> keyList = new List<string>( );
+            Dictionary<string, string> mappedTable = new Dictionary<string, string>( );
+
+            int columnsInSheet  = worksheet.Dimension.End.Column;
+            int rowsInSheet     = worksheet.Dimension.End.Row;
+
+            for (int currentRow = 1; currentRow <= rowsInSheet; currentRow++)
+            {
+                for (int currentColumn = 1; currentColumn <= columnsInSheet; currentColumn++)
+                {
+                    // do stuff
+                }
+            }
+            return mappedTable;
         }
     }    
 }
