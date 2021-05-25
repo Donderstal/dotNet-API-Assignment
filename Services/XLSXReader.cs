dba2c7b96c0529a70ad5f0e0ccf7c83b64b519f0
Donderstal <daanonderstal@hotmail.com>
2021-05-25 09:20:56 +0200

    basic methods and props for XLSXReader

--- Services/XLSXReader.cs
+++ Services/XLSXReader.cs
@@ -1,5 +1,25 @@
+using Microsoft.AspNetCore.Http;
+
 namespace XLSXReaderAPI.Services {
     public class XLSXReader {
-        private bool fileIsSet { get; set; }
+        private bool FileIsSet { get; set; }
+        private string FilePath { get; set; }
+        private IFormFile XLSXFile { get; set; }
+        public XLSXReader( )
+        {
+            FileIsSet = false;
+        }
+        private void setFileToReader( IFormFile file, string filePath )
+        {
+            XLSXFile = file;
+            FilePath = filePath;
+            FileIsSet = true; 
+        }
+        private void unsetFileFromReader( )
+        {
+            XLSXFile = null;
+            FilePath = null;
+            FileIsSet = false; 
+        }
     }    
 }
