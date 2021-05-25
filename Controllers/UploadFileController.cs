065c2539a6d038ff4414a286ba996e8e0eeb7c23
Donderstal <daanonderstal@hotmail.com>
2021-05-25 08:36:19 +0200

    cleaning unused code

--- Controllers/UploadFileController.cs
+++ Controllers/UploadFileController.cs
@@ -1,12 +1,10 @@
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
-namespace TestApi2.Controllers
+namespace XLSXReaderAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
